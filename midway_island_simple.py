611befc56404858436dfcb43cf00d8b5db9a77cc
SonT <sontru@googlemail.com>
2020-04-18 15:01:06 +0100

    Translated the blurb

--- midway_island_simple.py
+++ midway_island_simple.py
@@ -2,8 +2,12 @@
 """
 A Simple Version of Midway Island Naval Warfare with Python for Easy Learning by lixingqiu
 这个射击游戏是使用Arcade模块制作的。这个版本是核心版本，演示了爆炸效果，类的继承，按键检测等。
+This shooting game is made using Arcade module. This version is the core version and demonstrates explosion effects, class inheritance, and key detection.
 其中滚动的背景是使用两张图片制作的，它们相隔SCREEN_HEIGHT的高度。如果图片的top到了最底下，也就是坐标为0了，那么它就瞬间移到最顶上去。
+The scrolling background is made using two pictures, which are separated by the height SCREEN_HEIGHT. If the top of the picture reaches the bottom, that is, the coordinate is 0, then it will move instantaneously to the top.
 要注意Arcade的坐标系统和数学中的坐标系统一样，在本游戏中是以左下角为原点，y轴正向上。在Pygame中则是左上角为原点，y轴正向下。
+It should be noted that the coordinate system of Arcade is the same as the coordinate system in mathematics. In this game, the lower left corner is used as the origin, and the y axis is upward. In Pygame, the upper left corner is the origin, and the y-axis is positive > bottom.
+
 """
 
 __author__ = "lixingqiu"
@@ -19,7 +23,7 @@ SPRITE_SCALING = 1.0
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "A Simple Version of Midway Island Naval Warfare with Pthon for Easy Learning(Python中途岛海战街机模拟)"
+SCREEN_TITLE = "A Simple Version of Midway Island Naval Warfare with Python for Easy Learning Python (中途岛海战街机模拟)"
 ENEMY_COUNT = 100
 MOVEMENT_SPEED  = 5
 
