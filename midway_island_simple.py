e945383fc4416e6895209616bba1d79bdf24b3b7
SonT <sontru@googlemail.com>
2020-04-19 14:14:59 +0100

    Set up player class

--- midway_island_simple.py
+++ midway_island_simple.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 A Simple Version of Midway Island Naval Warfare with Python for Easy Learning by lixingqiu
-这个射击游戏是使用Arcade模块制作的。这个版本是核心版本，演示了爆炸效果，类的继承，按键检测等。
 This shooting game is made using Arcade module. This version is the core version and demonstrates explosion effects, class inheritance, and key detection.
-其中滚动的背景是使用两张图片制作的，它们相隔SCREEN_HEIGHT的高度。如果图片的top到了最底下，也就是坐标为0了，那么它就瞬间移到最顶上去。
 The scrolling background is made using two pictures, which are separated by the height SCREEN_HEIGHT. If the top of the picture reaches the bottom, that is, the coordinate is 0, then it will move instantaneously to the top.
-要注意Arcade的坐标系统和数学中的坐标系统一样，在本游戏中是以左下角为原点，y轴正向上。在Pygame中则是左上角为原点，y轴正向下。
 It should be noted that the coordinate system of Arcade is the same as the coordinate system in mathematics. In this game, the lower left corner is used as the origin, and the y axis is upward. In Pygame, the upper left corner is the origin, and the y-axis is positive > bottom.
 
 """
 
-__author__ = "lixingqiu"
+__author__ = "lixingqiu/sontru"
 __date__ = "2019/3/13"
 __url__ = "http://www.lixingqiu.com/?p=209"
 __qq__ = "406273900"
@@ -23,17 +20,32 @@ SPRITE_SCALING = 1.0
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "A Simple Version of Midway Island Naval Warfare with Python for Easy Learning Python (中途岛海战街机模拟)"
+SCREEN_TITLE = "A Simple Version of Midway Island Naval Warfare with Python for Easy Learning Python"
 ENEMY_COUNT = 100
 MOVEMENT_SPEED  = 5
 
+class Player(arcade.Sprite):
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+
 class Explosion(arcade.Sprite):
-    """ 创建可爆炸的角色 create explosion sprite""" 
+    """ create explosion sprite""" 
 
     def __init__(self, texture_list,x,y):
-        """texture_list是已经load了的造型列表"""
+        """texture_list load"""
         super().__init__()
-
         self.center_x = x
         self.center_y = y
         # 第一帧
@@ -59,7 +71,7 @@ class Bullet(arcade.Sprite):
         self.change_y = 20        
 
     def update(self):
-        """每帧更新坐标 update coordinates"""
+        """ update coordinates"""
         self.center_x += self.change_x
         self.center_y += self.change_y
         if self.bottom >  SCREEN_HEIGHT: self.kill()
@@ -84,7 +96,6 @@ class Enemy(arcade.Sprite):
     def update(self):
         
         if self.top<=0:self.kill()
-            
         
 class MyGame(arcade.Window):
     """ 应用程序主要类. """
@@ -102,10 +113,6 @@ class MyGame(arcade.Window):
         self.player_sprite = None
 
         self.enemy_explosion_images = []
-#        for i in range(55):
-#            # 加载从 explosion0000.png 到 explosion0054.png 的所有图片为敌人的爆炸效果动画帧            
-#            texture_name = "images/enemy_explosion/explosion{x:04d}.png".format(x=i)
-#            self.enemy_explosion_images.append(arcade.load_texture(texture_name))             
 
         columns = 16
         count = 60
@@ -130,7 +137,7 @@ class MyGame(arcade.Window):
         
         # 设置玩家操作的飞机
         self.score = 0
-        self.player_sprite = arcade.AnimatedTimeSprite("images/midway/Plane 1.png",SPRITE_SCALING)
+        self.player_sprite = Player("images/midway/Plane 1.png",SPRITE_SCALING)
         self.player_sprite.textures.append(arcade.load_texture("images/midway/Plane 2.png"))
         self.player_sprite.textures.append(arcade.load_texture("images/midway/Plane 3.png"))
         self.player_sprite.scale = 1.0
@@ -146,7 +153,7 @@ class MyGame(arcade.Window):
             self.all_sprites_list.append(enemy)
             
         sea_image = "images/midway/sea.png"
-        # Setting a Fixed Background 设置不动的背景 To cover up rolling background cracks
+        # Setting a Fixed Background To cover up rolling background cracks
         self.background = arcade.Sprite(sea_image)
         self.background.center_x = SCREEN_WIDTH // 2
         self.background.bottom = 0
@@ -171,7 +178,7 @@ class MyGame(arcade.Window):
         
         
     def on_key_press(self, key, modifiers):
-        """鼠标按键事件 """
+        """ Mouse button events """
         if self.player_sprite.health <=0 :return
         if key == arcade.key.W:
             self.player_sprite.change_y = MOVEMENT_SPEED
@@ -188,7 +195,7 @@ class MyGame(arcade.Window):
             self.all_sprites_list.append(self.bullet)            
 
     def on_key_release(self, key, modifiers):
-        """鼠标松开事件 """
+        """ Mouse release event """
         if self.player_sprite.health <=0 :return
         if key == arcade.key.W or key == arcade.key.S:
             self.player_sprite.change_y = 0
@@ -197,12 +204,12 @@ class MyGame(arcade.Window):
 
 
     def update(self, delta_time):
-        """ 游戏更新逻辑"""
+        """ Game update logic """
 
-        # 更新坐标等等
+        # Update coordinates, etc.
         #  print(self.background2.bottom - self.background1.top)
         distance = self.background2.bottom - self.background1.top
-        if  distance < 0 and distance > -40:   # Fissure remedy 弥补裂隙问题
+        if  distance < 0 and distance > -40:   # Fissure remedy - Make up for the cracks
              
             self.background2.bottom = self.background1.top
             
@@ -211,9 +218,9 @@ class MyGame(arcade.Window):
         self.player_sprite.update_animation()         
 
         if self.player_sprite.health > 0:
-            # 玩家和所有敌机的碰撞检测.
+            # Collision detection between players and all enemy aircraft.
             hit_list = arcade.check_for_collision_with_list(self.player_sprite,self.enemy_list)
-            # 遍历碰到的敌机列表
+            # Traverse the list of enemy aircraft encountered
             for enemy in hit_list:
                 enemy.kill()
                 self.player_sprite.health -= 1
@@ -236,7 +243,7 @@ class MyGame(arcade.Window):
             hit_list = arcade.check_for_collision_with_list(enemy,self.bullet_list)
             if len(hit_list) > 0:
                 enemy.kill()
-                [b.kill() for b in hit_list]   # 碰到的每颗子弹都删除
+                [b.kill() for b in hit_list]   # Delete every bullet encountered
                 self.score += len(hit_list)                
                 e = Explosion(self.enemy_explosion_images,enemy.center_x,enemy.center_y)
                 e.center_x = enemy.center_x
@@ -246,14 +253,14 @@ class MyGame(arcade.Window):
                 self.all_sprites_list.append(e)
                 
     def on_draw(self):
-        """   渲染屏幕    """
+        """ Render screen """
 
-        # 开始渲染，此命令要在所有重画命令之前
+        # Start rendering, this command must be before all redraw commands
         arcade.start_render()
-        self.background.draw()    # 画不动的背景，弥补滚动背景裂纹问题
-        self.background1.draw()   # 画滚动背景
-        self.background2.draw()   # 画滚动背景
-        # 画所有的角色
+        self.background.draw()    # Unmovable background to make up for the problem of rolling background cracks
+        self.background1.draw()   # Paint scroll background
+        self.background2.draw()   # Paint scroll background
+        # Draw all the characters
         self.enemy_list.draw()
         if self.player_sprite.health > 0:            
            self.player_sprite.draw()
@@ -261,7 +268,7 @@ class MyGame(arcade.Window):
         self.enemy_explosion_list.draw()
         
         # 画得分情况
-        score = "score: " + str(self.score) + " , Blood volume: " + str(self.player_sprite.health)
+        score = "Kills: " + str(self.score) + ", Lives: " + str(self.player_sprite.health)
         arcade.draw_text(score, 10, 20, arcade.color.WHITE, 14 )
 
         if self.player_sprite.health < 1:  # 血量小于1显示游戏结束
@@ -282,8 +289,8 @@ class Cover(arcade.Window):
         self.demo_plane = arcade.AnimatedTimeSprite("images/midway/Plane 1.png",0.8)
         self.demo_plane.textures.append(arcade.load_texture("images/midway/Plane 2.png"))
         self.demo_plane.textures.append(arcade.load_texture("images/midway/Plane 3.png"))
-        self.demo_plane.scale = 0.8
-        self.demo_plane.center_x = 300
+        self.demo_plane.scale = 3.0
+        self.demo_plane.center_x = 650
         self.demo_plane.center_y = 100
         self.begin_music = arcade.sound.load_sound("images/midway/start.wav")
         arcade.sound.play_sound(self.begin_music)
@@ -291,7 +298,7 @@ class Cover(arcade.Window):
     def on_key_press(self, key, modifiers):
         """鼠标按键事件 """
 
-        if key == arcade.key.SPACE: # 按空格键关闭窗口
+        if key == arcade.key.SPACE: # Press the space bar to close the window
             self.close()
                                         
     def update(self,delta_time):
@@ -310,7 +317,7 @@ class Cover(arcade.Window):
 
 def main():
 
-    cover = Cover(800, 600, SCREEN_TITLE)
+    cover = Cover(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     cover.setup()
     arcade.run()
     
